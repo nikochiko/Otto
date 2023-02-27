9d383f0f12ca3401b6b0e698f7705305d1359319
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2023-02-28 00:11:51 +0530

    correct sqlite query vars

--- otto/db.py
+++ otto/db.py
@@ -6,23 +6,28 @@ db_path = "otto.db"
 
 def get_value(category, id):
     con = sqlite3.connect(db_path)
+    key = make_key(category, id)
     result = con.execute(
         "SELECT value FROM thing WHERE key=?",
-        make_key(category, id))
+        (key,)
+    )
     row = result.fetchone()
     return row and row.value or None
 
 
 def set_value(category, id, value):
     con = sqlite3.connect(db_path)
+    key = make_key(category, id)
     if get_value(category, id) is not None:
         con.execute(
             "UPDATE thing SET value = ? WHERE key = ?",
-            value, make_key(category, id))
+            (value, key)
+        )
     else:
         con.execute(
             "INSERT INTO thing (key, value) VALUES (?, ?)",
-            make_key(category, id), value)
+            (key, value)
+        )
 
 
 def make_key(category, id):
